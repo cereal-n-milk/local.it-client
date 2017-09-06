eccb626e5cc910c312d07812613bc1d41edf5a53
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-09-05 21:26:35 -0700

    Map over an array of values and render each to the Discover component

--- components/Discover.js
+++ components/Discover.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-const Discover = () => (
-  <Text style={styles.toolbarTab}>
-    Discover
-  </Text>
-)
+/* Import categories from API to categories array */
+
+class Discover extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = { discoveries: ['Christine', 'Francis', 'Nick', 'Schaefer'] };
+  }
+
+  render () {
+    return (
+      <View>{this.state.discoveries.map(person => {
+        return (
+        <View><Text>{person}</Text></View>
+        )
+      })}
+      </View>
+    )
+  }
+
+}
 
 const styles = StyleSheet.create({
   toolbarTab: {
@@ -14,3 +31,9 @@ const styles = StyleSheet.create({
 })
 
 export default Discover
+
+//From Francis Ngo to Everyone: (07:39 PM)
+// {array.forEach((category) => { <Link to=“”><CategoryView/></Link> }
+    // <Text style={styles.toolbarTab}>
+    //   Discover
+    // </Text>
