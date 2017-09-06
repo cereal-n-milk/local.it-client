c6366b4510caf07a0f1e7e631fa836ac41a15490
Francis Ngo <francis.t.ngo@gmail.com>
2017-09-06 08:05:06 -0700

    Add hardcode data to display Discover categories

--- components/Discover.js
+++ components/Discover.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, FlatList } from 'react-native';
 import { Link } from 'react-router-native';
 
 /* Import categories from API to categories array */
@@ -9,28 +9,47 @@ class Discover extends Component {
   constructor (props) {
     super(props);
 
-    this.state = { discoveries: ['Moab', 'San Francisco', 'Boulder', 'Banff'] };
+    this.state = {
+      discoveries: [
+        {
+          title: 'Active Life'
+        },
+        {
+          title: 'Arts & Entertainment'
+        },
+        {
+          title: 'Beauty & Spa'
+        },
+        {
+          title: 'Local Flavor'
+        },
+        {
+          title: 'Nightlife'
+        },
+        {
+          title: 'Restaurants'
+        },
+        {
+          title: 'Shopping'
+        }
+      ]
+    };
   }
 
   render () {
-    const locationsList =
-      this.state.discoveries.map(location => {
-        return (
-          <Link
-            to=""
-            underlayColor=''
-            style={styles.locationItem}>
-              <Text>{location}</Text>
-          </Link>
-        )})
-
     return (
-      <View>
-        {locationsList}
+      <View style={styles.container}>
+          <FlatList
+            data={this.state.discoveries}
+            renderItem={({ item }) =>
+            <Link to="" style={styles.locationItem}>
+              <Text>{item.title}</Text>
+            </Link>
+            }
+          />
       </View>
     )
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -42,12 +61,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 15,
+    padding: 25,
     color: '#596A7F'
   }
 })
 
-export default Discover
+export default Discover;
 
 //From Francis Ngo to Everyone: (07:39 PM)
 // {array.forEach((category) => { <Link to=“”><CategoryView/></Link> }
