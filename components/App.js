e0ebce1432749008ba4b81da4d84e8b3364b9145
Scott Schaefer <scottschaef@gmail.com>
2017-09-06 18:45:55 -0400

    Create ability to get latitude and longitude of client

--- components/App.js
+++ components/App.js
@@ -11,8 +11,8 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Toolbar />
         {/* <Login/> */}
+        <Toolbar />
       </View>
     );
   }
