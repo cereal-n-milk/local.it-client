1f5d1d8c86b395053c7d718099d4bd8e17b8ea6f
Francis Ngo <francis.t.ngo@gmail.com>
2017-09-07 08:50:59 -0700

    WIP - Set up StackNavigator for nested routes

--- components/config/router.js
+++ components/config/router.js
@@ -6,10 +6,29 @@ import Discover from '../Discover';
 import Saved from '../Saved';
 import Itinerary from '../Itinerary';
 import Profile from '../Profile';
+import CategoryView from '../CategoryView';
+
+export const CategoryStack = StackNavigator({
+  Discover: {
+    screen: Discover,
+    navigationOptions: {
+      title: 'Discover'
+    },
+  },
+  CategoryView: {
+    screen: CategoryView,
+    navigationOption: {
+      //TODO: Finish connecting CategoryView to Discover
+      //get data.title from Discover to display instead of hard code
+      //title: ({ state }) => `${state.params.title.toUpperCase()}`
+      title: 'Active Life'
+    }
+  },
+});
 
 export const Tabs = TabNavigator({
   Discover: {
-    screen: Discover
+    screen: CategoryStack
   },
   Saved : {
     screen: Saved
