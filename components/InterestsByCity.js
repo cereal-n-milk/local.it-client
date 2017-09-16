638ad3dbcc511084d28b1377a3598cab7955f8ac
Francis Ngo <francis.t.ngo@gmail.com>
2017-09-15 19:37:12 -0700

    Edit InterestsByCity component styles and install react-native-elements

--- components/InterestsByCity.js
+++ components/InterestsByCity.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, Image, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 
 export default class InterestByCity extends Component {
   constructor (props) {
     super(props);
     this.state = {};
   }
-  // TODO: render each interest individually
-  // render a list of all Saved-Interrests
   render() {
     let city = this.props.navigation.state.params.interests.city;
     let interests = this.props.navigation.state.params.interests[0].interests;
     return (
       <ScrollView>
-        <Text>{city}</Text>
         <FlatList
           data={interests}
           keyExtractor={(interest, index) => index }
-          renderItem={({ item }) =>
-            <TouchableOpacity
-              style={styles.interestItem}
-              onPress={ () => console.log('woot') }>
-                <View>
-                  <Image
-                    style={{width: 100, height: 100}}
-                    source={{ uri: item.image_url}} />
-                  <Text>{item.name}</Text>
-                  <Text>Rating: {item.rating}</Text>
-                  <Text>Distance: {item.distance}</Text>
-                  <Text>Price: {item.price}</Text>
-                </View>
-            </TouchableOpacity>
+          renderItem={ ({ item }) =>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{width: 100, height: 100}}>
+              <Image
+                style={{width: 100, height: 100}}
+                source={{ uri: item.image_url}}
+              />
+            </View>
+            <View style={{width: 200, marginLeft: 10}}>
+              <Text style={{fontWeight: 'bold'}}>{item.name}</Text>
+              <Text>{item.location.address1}</Text>
+              <Text>{item.location.city} {item.location.state}, {item.location.zip_code}</Text>
+              <Text>Rating: {item.rating}</Text>
+              <Text>Category: {item.categories[0].title}</Text>
+            </View>
+            <View style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: 50
+            }}>
+              <CheckBox
+                center
+                style={{backgroundColor: '#eaeaea'}}
+                checked={this.state.checked}
+              />
+            </View>
+          </View>
           }
         />
       </ScrollView>
@@ -39,13 +51,4 @@ export default class InterestByCity extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  interestItem: {
-
-  }
 })
