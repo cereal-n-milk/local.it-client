1c6f1a1ba7074ac0c88f22fa0fa2452204488e3a
Francis Ngo <francis.t.ngo@gmail.com>
2017-09-15 19:36:28 -0700

    Switch back to fetching data without ML

--- components/Discover.js
+++ components/Discover.js
@@ -14,7 +14,7 @@ export default class Discover extends Component {
       latitude: null,
       longitude: null,
       error: null,
-       categories: categories,
+      categories: categories,
     };
   }
 
@@ -34,63 +34,87 @@ export default class Discover extends Component {
   }
 
   //Yelp Fetch that goes through Python
+  // fetchYelpData (title) {
+  //   const credentials = {
+  //     appId: YelpConfig.appId,
+  //     appSecret: YelpConfig.appSecret
+  //   };
+  //   console.log('props: ', this.props);
+  //   const yelp = new YelpApi(credentials);
+  //   let lat = this.state.latitude;
+  //   let lng = this.state.longitude;
+  //   let latlng = String(lat) + ',' + String(lng);
+  //   let userdata = null;
+  //   let userid = this.props.screenProps.fbID;
+  //   let params = {
+  //     term: title,
+  //     location: latlng,
+  //     limit: '15',
+  //   };
+  //
+  //   fetch(`http://localhost:3000/api/${userid}`, {
+  //         method: 'GET',
+  //         headers: {'Content-Type': 'application/json'},
+  //       })
+  //   .then((response) => {
+  //     userdata = response;
+  //     yelp.search(params)
+  //     .then((data) => {
+  //       var usercheck = JSON.parse(userdata._bodyInit);
+  //       console.log('yelp data: ', data);
+  //       console.log('userdata: ', usercheck);
+  //       if (usercheck.interestsByCity.length > 0) {
+  //         if (usercheck.interestsByCity[0].interests.length > 3 && usercheck.interestsByCity[0].dislikedInterests.length > 3) {
+  //             fetch('http://localhost:3000/python', {
+  //               method: 'POST',
+  //               headers: {'Content-Type': 'application/json'},
+  //               body: JSON.stringify({yelp: data, user: userdata._bodyInit})
+  //             })
+  //             .then((data) => {
+  //               var target = JSON.parse(data._bodyInit);
+  //               target = JSON.parse(target[0]);
+  //               console.log('retreived from python api: ', target);
+  //               this.props.navigation.navigate('CategoryView', {
+  //                 data: target,
+  //                 category: title });
+  //             })
+  //             .catch((err) => console.log(err));
+  //         } else {
+  //           this.props.navigation.navigate('CategoryView', {
+  //           data: data.businesses,
+  //           category: title });
+  //         }
+  //       } else {
+  //         this.props.navigation.navigate('CategoryView', {
+  //         data: data.businesses,
+  //         category: title });
+  //       }
+  //     });
+  //   });
+  // }
+
   fetchYelpData (title) {
     const credentials = {
       appId: YelpConfig.appId,
       appSecret: YelpConfig.appSecret
-    };
-    console.log('props: ', this.props);
+    }
     const yelp = new YelpApi(credentials);
     var lat = this.state.latitude;
     var lng = this.state.longitude;
     var latlng = String(lat) + ',' + String(lng);
-    var userdata = null;
-    var userid = this.props.screenProps.fbID;
     let params = {
       term: title,
       location: latlng,
-      limit: '15',
+      limit: '30',
     };
-
-    fetch(`http://localhost:3000/api/${userid}`, {
-          method: 'GET',
-          headers: {'Content-Type': 'application/json'},
-        })
-    .then((response) => {
-      userdata = response;
-      yelp.search(params)
+    yelp.search(params)
       .then((data) => {
-        var usercheck = JSON.parse(userdata._bodyInit);
-        console.log('yelp data: ', data);
-        console.log('userdata: ', usercheck);
-        if (usercheck.interestsByCity.length > 0) {
-          if (usercheck.interestsByCity[0].interests.length > 3 && usercheck.interestsByCity[0].dislikedInterests.length > 3) {
-              fetch('http://localhost:3000/python', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({yelp: data, user: userdata._bodyInit})
-              })
-              .then((data) => {
-                var target = JSON.parse(data._bodyInit);
-                target = JSON.parse(target[0]);
-                console.log('retreived from python api: ', target);
-                this.props.navigation.navigate('CategoryView', {
-                  data: target,
-                  category: title });
-              })
-              .catch((err) => console.log(err));
-          } else {
-            this.props.navigation.navigate('CategoryView', {
-            data: data.businesses,
-            category: title });
-          }
-        } else {
-          this.props.navigation.navigate('CategoryView', {
+        this.props.navigation.navigate('CategoryView', {
           data: data.businesses,
-          category: title });
-        }
-      });
-    });
+          category: title })
+        //console.log('State: ',this.state);
+      })
+      .catch((err) => console.log(err));
   }
 
   render () {
