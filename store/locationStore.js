0537afda1736384bee073385a44656cdfb21e31b
Francis Ngo <francis.t.ngo@gmail.com>
2017-09-19 18:06:30 -0700

    Add Redux store to localit application

---
+++ store/locationStore.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+
+const defaultState = {
+  latitude: null,
+  longitude: null,
+}
+
+function locationStore(state = defaultState, action) {
+  console.log('accessing state:', action);
+  switch (action.type) {
+    case 'GET_LOCATION':
+      return Object.assign({}, state, {
+        latitude: action.latitude,
+        longitude: action.longitude,
+      });
+    default:
+      return state;
+  }
+}
+
+export default createStore(locationStore);
